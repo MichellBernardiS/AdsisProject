e3a3c05592539d6ff0f31d22e0331f80c4d6bdce
Michell Bernardi <michell.bernardi@ti.ukdw.ac.id>
2017-10-11 15:10:17 +0700

    Upgrade
    Upgrade

--- script.sh
+++ script.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-echo "Details about your OS : "
-uname -s -r -p -i
-
+echo -e "Details about your OS : \nYour kernel name : \c"
+uname -s 
+echo -e "Operating system : \c"
+uname -o
+echo -e "Kernel release at : \c"
+uname -r 
+echo -e "Kernel processor : \c"
+uname -p 
+echo -e "Hardware platform : \c"
+uname -i
+echo -e "Hardware machine : \c"
+uname -m
 echo "Your distribution OS information : "
 cat /etc/*-release
+
