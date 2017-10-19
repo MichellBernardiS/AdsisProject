b6e6f428569dc8ab1fb08f2b53c35c0dd5fd1436
Michell Bernardi <michell.bernardi@ti.ukdw.ac.id>
2017-10-20 01:44:27 +0700

    Penambahan last logged in user dan aktivitas yang dilakukan user tersebut
    Kurang gabungin script..
    Penting.. Yg menjalankan script ini harap pake command source, jangan sh atau ./

--- script.sh
+++ script.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-echo -e "Details about your OS : \nYour kernel name : \c"
-uname -s 
-echo -e "Operating system : \c"
-uname -o
-echo -e "Kernel release at : \c"
-uname -r 
-echo -e "Kernel processor : \c"
-uname -p 
-echo -e "Hardware platform : \c"
-uname -i
-echo -e "Hardware machine : \c"
-uname -m
-echo "Your distribution OS information : "
-cat /etc/*-release
+# please using source command like source script.sh instead of sh or ./
+# we got idea from https://github.com/atarallo/TECMINT_MONITOR
 
+echo -e "$(tput setaf 2)Details about your OS : \nYour kernel name : $(tput setaf 7)\c"
+uname -s 
+echo -e "$(tput setaf 2)Operating system : $(tput setaf 7)\c"
+uname -o
+echo -e "$(tput setaf 2)Kernel release at : $(tput setaf 7)\c"
+uname -r 
+echo -e "$(tput setaf 2)Kernel processor : $(tput setaf 7)\c"
+uname -p 
+echo -e "$(tput setaf 2)Hardware platform : $(tput setaf 7)\c"
+uname -i
+
+echo -e "$(tput setaf 2)Logged in users : $(tput setaf 7)\n"
+finger -l
+echo ""
+
+cat "$HOME/.bash_history" > backup.txt
+echo "Commands which users type can be looked in backup.txt file"
+
+echo -e "$(tput setaf 2)\n Last 10 user commands : $(tput setaf 7)"
+shopt -s histappend
+history -a
+history 10
