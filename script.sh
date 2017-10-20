3a017e44f966cebb9fc3397ce836d78760e50a58
Michell Bernardi <michell.bernardi@ti.ukdw.ac.id>
2017-10-20 23:21:32 +0700

    File yang jadi
    Kombinasi coding dari teman satu kelompok.. Untuk execute command, harap menggunakan perintah source <namaFilenya>. Terimakasih

--- script.sh
+++ script.sh
@@ -13,6 +13,28 @@ echo -e "$(tput setaf 2)Kernel processor : $(tput setaf 7)\c"
 uname -p 
 echo -e "$(tput setaf 2)Hardware platform : $(tput setaf 7)\c"
 uname -i
+echo ""
+#print memory usage 
+free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+echo ""
+#print cpu usage
+top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}'
+echo "" 
+
+#citing from another source and adding some commends
+echo "$(tput setaf 2)Use%  Avail  Used  Size  Filesystem $(tput setaf 7)"
+df -H | grep -vE '^Filesystem' | awk '{print $5 " " $4 " " $3 " " $2 " " $1}' | while read output
+
+do
+    echo $output
+    usep=$(echo $output | awk '{print $1}' | cut -d'%' -f1)
+    partition=$(echo $output | awk '{print $2}')
+    if [ $usep -ge 75 ]; then
+       echo "Running out of space \"$partition ($usep%)\" on $(hostname)"
+    fi
+done
+
+#source : https://www.cyberciti.biz/tips/shell-script-to-watch-the-disk-space.html
 
 echo -e "$(tput setaf 2)Logged in users : $(tput setaf 7)\n"
 finger -l
